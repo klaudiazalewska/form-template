08825e9138f89e5d8a7a6b900f99579a1ac9f73a
klaudiazalewska <73396554+klaudiazalewska@users.noreply.github.com>
2022-03-28 19:34:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+function isFilled(input) {
+    if(input.login.value == "") {
+       alert("źle");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
+
+
+window.onload = function () {
+    document.getElementById("article").onsubmit = function () {
+        if (isFilled(this.name)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+}
